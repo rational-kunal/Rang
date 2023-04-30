84410708ee43df807ebd38cd82a75380c25e5677
Kunal Kamble <kunalgk1999@gmail.com>
2023-04-30 16:50:56 +0530

    ✨ Added extended color

--- Example/Rang/ViewController.swift
+++ Example/Rang/ViewController.swift
@@ -9,15 +9,230 @@
 import Rang
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    let tableView = UITableView(frame: .zero)
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .random
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        ])
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITableViewDataSource
+
+    func numberOfSections(in _: UITableView) -> Int {
+        // Sections:
+        // 1st -> UIKit Fixed Colors
+        // 2nd -> Rang Extended Colors
+        return 2
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 { return UIKitFixedColorAndName.count }
+        else if section == 1 { return rangColorAndName.count }
+        return 0
+    } 
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")!
+
+        var colorDetail: ColorDetail?
+        if indexPath.section == 0 { colorDetail = UIKitFixedColorAndName[indexPath.row] }
+        else if indexPath.section == 1 { colorDetail = rangColorAndName[indexPath.row] }
+
+        var content = cell.defaultContentConfiguration()
+        content.image = colorDetail?.color.image(CGSizeMake(84, 32))
+        content.imageProperties.cornerRadius = 4
+        content.text = colorDetail?.name
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+    func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 { return "UIKit Fixed Colors" }
+        else if section == 1 { return "Rang Extended Colors" }
+        return "Unknown"
     }
 }
+
+// MARK: UIColor to image
+
+extension UIColor {
+    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+
+// MARK: Color and Name lists
+
+struct ColorDetail {
+    var color: UIColor
+    var name: String
+}
+
+/// https://developer.apple.com/documentation/uikit/uicolor/standard_colors#3174519
+let UIKitFixedColorAndName = [ColorDetail(color: .black, name: "black"),
+                              ColorDetail(color: .blue, name: "blue"),
+                              ColorDetail(color: .brown, name: "brown"),
+                              ColorDetail(color: .cyan, name: "cyan"),
+                              ColorDetail(color: .darkGray, name: "darkGray"),
+                              ColorDetail(color: .gray, name: "gray"),
+                              ColorDetail(color: .green, name: "green"),
+                              ColorDetail(color: .lightGray, name: "lightGray"),
+                              ColorDetail(color: .magenta, name: "magenta"),
+                              ColorDetail(color: .orange, name: "orange"),
+                              ColorDetail(color: .purple, name: "purple"),
+                              ColorDetail(color: .red, name: "red"),
+                              ColorDetail(color: .white, name: "white"),
+                              ColorDetail(color: .yellow, name: "yellow")]
+
+/// https://www.w3.org/wiki/CSS/Properties/color/keywords
+let rangColorAndName = [ColorDetail(color: .silver, name: "silver"),
+                        ColorDetail(color: .maroon, name: "maroon"),
+                        ColorDetail(color: .fuchsia, name: "fuchsia"),
+                        ColorDetail(color: .lime, name: "lime"),
+                        ColorDetail(color: .olive, name: "olive"),
+                        ColorDetail(color: .navy, name: "navy"),
+                        ColorDetail(color: .teal, name: "teal"),
+                        ColorDetail(color: .aqua, name: "aqua"),
+                        ColorDetail(color: .aliceBlue, name: "aliceBlue"),
+                        ColorDetail(color: .antiqueWhite, name: "antiqueWhite"),
+                        ColorDetail(color: .aquamarine, name: "aquamarine"),
+                        ColorDetail(color: .azure, name: "azure"),
+                        ColorDetail(color: .beige, name: "beige"),
+                        ColorDetail(color: .bisque, name: "bisque"),
+                        ColorDetail(color: .blanchedAlmond, name: "blanchedAlmond"),
+                        ColorDetail(color: .blueViolet, name: "blueViolet"),
+                        ColorDetail(color: .burlyWood, name: "burlyWood"),
+                        ColorDetail(color: .cadetBlue, name: "cadetBlue"),
+                        ColorDetail(color: .chartreuse, name: "chartreuse"),
+                        ColorDetail(color: .chocolate, name: "chocolate"),
+                        ColorDetail(color: .coral, name: "coral"),
+                        ColorDetail(color: .cornflowerBlue, name: "cornflowerBlue"),
+                        ColorDetail(color: .cornsilk, name: "cornsilk"),
+                        ColorDetail(color: .crimson, name: "crimson"),
+                        ColorDetail(color: .darkBlue, name: "darkBlue"),
+                        ColorDetail(color: .darkCyan, name: "darkCyan"),
+                        ColorDetail(color: .darkGoldenRod, name: "darkGoldenRod"),
+                        ColorDetail(color: .darkGreen, name: "darkGreen"),
+                        ColorDetail(color: .darkGrey, name: "darkGrey"),
+                        ColorDetail(color: .darkKhaki, name: "darkKhaki"),
+                        ColorDetail(color: .darkMagenta, name: "darkMagenta"),
+                        ColorDetail(color: .darkOliveGreen, name: "darkOliveGreen"),
+                        ColorDetail(color: .darkOrange, name: "darkOrange"),
+                        ColorDetail(color: .darkOrchid, name: "darkOrchid"),
+                        ColorDetail(color: .darkRed, name: "darkRed"),
+                        ColorDetail(color: .darkSalmon, name: "darkSalmon"),
+                        ColorDetail(color: .darkSeaGreen, name: "darkSeaGreen"),
+                        ColorDetail(color: .darkSlateBlue, name: "darkSlateBlue"),
+                        ColorDetail(color: .darkSlateGray, name: "darkSlateGray"),
+                        ColorDetail(color: .darkSlateGrey, name: "darkSlateGrey"),
+                        ColorDetail(color: .darkTurquoise, name: "darkTurquoise"),
+                        ColorDetail(color: .darkViolet, name: "darkViolet"),
+                        ColorDetail(color: .deepPink, name: "deepPink"),
+                        ColorDetail(color: .deepSkyBlue, name: "deepSkyBlue"),
+                        ColorDetail(color: .dimGray, name: "dimGray"),
+                        ColorDetail(color: .dimGrey, name: "dimGrey"),
+                        ColorDetail(color: .dodgerBlue, name: "dodgerBlue"),
+                        ColorDetail(color: .fireBrick, name: "fireBrick"),
+                        ColorDetail(color: .floralWhite, name: "floralWhite"),
+                        ColorDetail(color: .forestGreen, name: "forestGreen"),
+                        ColorDetail(color: .gainsboro, name: "gainsboro"),
+                        ColorDetail(color: .ghostWhite, name: "ghostWhite"),
+                        ColorDetail(color: .gold, name: "gold"),
+                        ColorDetail(color: .goldenRod, name: "goldenRod"),
+                        ColorDetail(color: .greenYellow, name: "greenYellow"),
+                        ColorDetail(color: .grey, name: "grey"),
+                        ColorDetail(color: .honeyDew, name: "honeyDew"),
+                        ColorDetail(color: .hotPink, name: "hotPink"),
+                        ColorDetail(color: .indianRed, name: "indianRed"),
+                        ColorDetail(color: .indigo, name: "indigo"),
+                        ColorDetail(color: .ivory, name: "ivory"),
+                        ColorDetail(color: .khaki, name: "khaki"),
+                        ColorDetail(color: .lavender, name: "lavender"),
+                        ColorDetail(color: .lavenderBlush, name: "lavenderBlush"),
+                        ColorDetail(color: .lawnGreen, name: "lawnGreen"),
+                        ColorDetail(color: .lemonChiffon, name: "lemonChiffon"),
+                        ColorDetail(color: .lightBlue, name: "lightBlue"),
+                        ColorDetail(color: .lightCoral, name: "lightCoral"),
+                        ColorDetail(color: .lightCyan, name: "lightCyan"),
+                        ColorDetail(color: .lightGoldenRodYellow, name: "lightGoldenRodYellow"),
+                        ColorDetail(color: .lightGreen, name: "lightGreen"),
+                        ColorDetail(color: .lightGrey, name: "lightGrey"),
+                        ColorDetail(color: .lightPink, name: "lightPink"),
+                        ColorDetail(color: .lightSalmon, name: "lightSalmon"),
+                        ColorDetail(color: .lightSeaGreen, name: "lightSeaGreen"),
+                        ColorDetail(color: .lightSkyBlue, name: "lightSkyBlue"),
+                        ColorDetail(color: .lightSlateGray, name: "lightSlateGray"),
+                        ColorDetail(color: .lightSlateGrey, name: "lightSlateGrey"),
+                        ColorDetail(color: .lightSteelBlue, name: "lightSteelBlue"),
+                        ColorDetail(color: .lightYellow, name: "lightYellow"),
+                        ColorDetail(color: .limeGreen, name: "limeGreen"),
+                        ColorDetail(color: .linen, name: "linen"),
+                        ColorDetail(color: .mediumAquamarine, name: "mediumAquamarine"),
+                        ColorDetail(color: .mediumBlue, name: "mediumBlue"),
+                        ColorDetail(color: .mediumOrchid, name: "mediumOrchid"),
+                        ColorDetail(color: .mediumPurple, name: "mediumPurple"),
+                        ColorDetail(color: .mediumSeaGreen, name: "mediumSeaGreen"),
+                        ColorDetail(color: .mediumSlateBlue, name: "mediumSlateBlue"),
+                        ColorDetail(color: .mediumSpringGreen, name: "mediumSpringGreen"),
+                        ColorDetail(color: .mediumTurquoise, name: "mediumTurquoise"),
+                        ColorDetail(color: .mediumVioletRed, name: "mediumVioletRed"),
+                        ColorDetail(color: .midnightBlue, name: "midnightBlue"),
+                        ColorDetail(color: .mintCream, name: "mintCream"),
+                        ColorDetail(color: .mistyRose, name: "mistyRose"),
+                        ColorDetail(color: .moccasin, name: "moccasin"),
+                        ColorDetail(color: .navajoWhite, name: "navajoWhite"),
+                        ColorDetail(color: .oldLace, name: "oldLace"),
+                        ColorDetail(color: .oliveDrab, name: "oliveDrab"),
+                        ColorDetail(color: .orangeRed, name: "orangeRed"),
+                        ColorDetail(color: .orchid, name: "orchid"),
+                        ColorDetail(color: .paleGoldenRod, name: "paleGoldenRod"),
+                        ColorDetail(color: .paleGreen, name: "paleGreen"),
+                        ColorDetail(color: .paleTurquoise, name: "paleTurquoise"),
+                        ColorDetail(color: .paleVioletRed, name: "paleVioletRed"),
+                        ColorDetail(color: .papayaWhip, name: "papayaWhip"),
+                        ColorDetail(color: .peachPuff, name: "peachPuff"),
+                        ColorDetail(color: .peru, name: "peru"),
+                        ColorDetail(color: .pink, name: "pink"),
+                        ColorDetail(color: .plum, name: "plum"),
+                        ColorDetail(color: .powderBlue, name: "powderBlue"),
+                        ColorDetail(color: .rosyBrown, name: "rosyBrown"),
+                        ColorDetail(color: .royalBlue, name: "royalBlue"),
+                        ColorDetail(color: .saddleBrown, name: "saddleBrown"),
+                        ColorDetail(color: .salmon, name: "salmon"),
+                        ColorDetail(color: .sandyBrown, name: "sandyBrown"),
+                        ColorDetail(color: .seaGreen, name: "seaGreen"),
+                        ColorDetail(color: .seaShell, name: "seaShell"),
+                        ColorDetail(color: .sienna, name: "sienna"),
+                        ColorDetail(color: .skyBlue, name: "skyBlue"),
+                        ColorDetail(color: .slateBlue, name: "slateBlue"),
+                        ColorDetail(color: .slateGray, name: "slateGray"),
+                        ColorDetail(color: .slateGrey, name: "slateGrey"),
+                        ColorDetail(color: .snow, name: "snow"),
+                        ColorDetail(color: .springGreen, name: "springGreen"),
+                        ColorDetail(color: .steelBlue, name: "steelBlue"),
+                        ColorDetail(color: .tan, name: "tan"),
+                        ColorDetail(color: .thistle, name: "thistle"),
+                        ColorDetail(color: .tomato, name: "tomato"),
+                        ColorDetail(color: .turquoise, name: "turquoise"),
+                        ColorDetail(color: .violet, name: "violet"),
+                        ColorDetail(color: .wheat, name: "wheat"),
+                        ColorDetail(color: .whiteSmoke, name: "whiteSmoke"),
+                        ColorDetail(color: .yellowGreen, name: "yellowGreen")]
--- Rang/Classes/Rang.swift
+++ Rang/Classes/Rang.swift
@@ -14,7 +14,8 @@ extension UIColor {
     }
 }
 
-// Random color
+// MARK: Random Color
+
 public extension UIColor {
     static var random: UIColor {
         let red = Int.random(in: 0 ... 255)
@@ -22,4 +23,142 @@ public extension UIColor {
         let blue = Int.random(in: 0 ... 255)
         return UIColor(red, green, blue)
     }
+}
+
+// MARK: CSS Standard Colors
+
+public extension UIColor {
+    static var silver: UIColor { return UIColor(192, 192, 192) }
+    static var maroon: UIColor { return UIColor(128, 0, 0) }
+    static var fuchsia: UIColor { return UIColor(255, 0, 255) }
+    static var lime: UIColor { return UIColor(0, 255, 0) }
+    static var olive: UIColor { return UIColor(128, 128, 0) }
+    static var navy: UIColor { return UIColor(0, 0, 128) }
+    static var teal: UIColor { return UIColor(0, 128, 128) }
+    static var aqua: UIColor { return UIColor(0, 255, 255) }
+    static var aliceBlue: UIColor { return UIColor(240, 248, 255) }
+    static var antiqueWhite: UIColor { return UIColor(250, 235, 215) }
+    static var aquamarine: UIColor { return UIColor(127, 255, 212) }
+    static var azure: UIColor { return UIColor(240, 255, 255) }
+    static var beige: UIColor { return UIColor(245, 245, 220) }
+    static var bisque: UIColor { return UIColor(255, 228, 196) }
+    static var blanchedAlmond: UIColor { return UIColor(255, 235, 205) }
+    static var blueViolet: UIColor { return UIColor(138, 43, 226) }
+    static var burlyWood: UIColor { return UIColor(222, 184, 135) }
+    static var cadetBlue: UIColor { return UIColor(95, 158, 160) }
+    static var chartreuse: UIColor { return UIColor(127, 255, 0) }
+    static var chocolate: UIColor { return UIColor(210, 105, 30) }
+    static var coral: UIColor { return UIColor(255, 127, 80) }
+    static var cornflowerBlue: UIColor { return UIColor(100, 149, 237) }
+    static var cornsilk: UIColor { return UIColor(255, 248, 220) }
+    static var crimson: UIColor { return UIColor(220, 20, 60) }
+    static var darkBlue: UIColor { return UIColor(0, 0, 139) }
+    static var darkCyan: UIColor { return UIColor(0, 139, 139) }
+    static var darkGoldenRod: UIColor { return UIColor(184, 134, 11) }
+    static var darkGreen: UIColor { return UIColor(0, 100, 0) }
+    static var darkGrey: UIColor { return UIColor(169, 169, 169) }
+    static var darkKhaki: UIColor { return UIColor(189, 183, 107) }
+    static var darkMagenta: UIColor { return UIColor(139, 0, 139) }
+    static var darkOliveGreen: UIColor { return UIColor(85, 107, 47) }
+    static var darkOrange: UIColor { return UIColor(255, 140, 0) }
+    static var darkOrchid: UIColor { return UIColor(153, 50, 204) }
+    static var darkRed: UIColor { return UIColor(139, 0, 0) }
+    static var darkSalmon: UIColor { return UIColor(233, 150, 122) }
+    static var darkSeaGreen: UIColor { return UIColor(143, 188, 143) }
+    static var darkSlateBlue: UIColor { return UIColor(72, 61, 139) }
+    static var darkSlateGray: UIColor { return UIColor(47, 79, 79) }
+    static var darkSlateGrey: UIColor { return UIColor(47, 79, 79) }
+    static var darkTurquoise: UIColor { return UIColor(0, 206, 209) }
+    static var darkViolet: UIColor { return UIColor(148, 0, 211) }
+    static var deepPink: UIColor { return UIColor(255, 20, 147) }
+    static var deepSkyBlue: UIColor { return UIColor(0, 191, 255) }
+    static var dimGray: UIColor { return UIColor(105, 105, 105) }
+    static var dimGrey: UIColor { return UIColor(105, 105, 105) }
+    static var dodgerBlue: UIColor { return UIColor(30, 144, 255) }
+    static var fireBrick: UIColor { return UIColor(178, 34, 34) }
+    static var floralWhite: UIColor { return UIColor(255, 250, 240) }
+    static var forestGreen: UIColor { return UIColor(34, 139, 34) }
+    static var gainsboro: UIColor { return UIColor(220, 220, 220) }
+    static var ghostWhite: UIColor { return UIColor(248, 248, 255) }
+    static var gold: UIColor { return UIColor(255, 215, 0) }
+    static var goldenRod: UIColor { return UIColor(218, 165, 32) }
+    static var greenYellow: UIColor { return UIColor(173, 255, 47) }
+    static var grey: UIColor { return UIColor(128, 128, 128) }
+    static var honeyDew: UIColor { return UIColor(240, 255, 240) }
+    static var hotPink: UIColor { return UIColor(255, 105, 180) }
+    static var indianRed: UIColor { return UIColor(205, 92, 92) }
+    static var indigo: UIColor { return UIColor(75, 0, 130) }
+    static var ivory: UIColor { return UIColor(255, 255, 240) }
+    static var khaki: UIColor { return UIColor(240, 230, 140) }
+    static var lavender: UIColor { return UIColor(230, 230, 250) }
+    static var lavenderBlush: UIColor { return UIColor(255, 240, 245) }
+    static var lawnGreen: UIColor { return UIColor(124, 252, 0) }
+    static var lemonChiffon: UIColor { return UIColor(255, 250, 205) }
+    static var lightBlue: UIColor { return UIColor(173, 216, 230) }
+    static var lightCoral: UIColor { return UIColor(240, 128, 128) }
+    static var lightCyan: UIColor { return UIColor(224, 255, 255) }
+    static var lightGoldenRodYellow: UIColor { return UIColor(250, 250, 210) }
+    static var lightGreen: UIColor { return UIColor(144, 238, 144) }
+    static var lightGrey: UIColor { return UIColor(211, 211, 211) }
+    static var lightPink: UIColor { return UIColor(255, 182, 193) }
+    static var lightSalmon: UIColor { return UIColor(255, 160, 122) }
+    static var lightSeaGreen: UIColor { return UIColor(32, 178, 170) }
+    static var lightSkyBlue: UIColor { return UIColor(135, 206, 250) }
+    static var lightSlateGray: UIColor { return UIColor(119, 136, 153) }
+    static var lightSlateGrey: UIColor { return UIColor(119, 136, 153) }
+    static var lightSteelBlue: UIColor { return UIColor(176, 196, 222) }
+    static var lightYellow: UIColor { return UIColor(255, 255, 224) }
+    static var limeGreen: UIColor { return UIColor(50, 205, 50) }
+    static var linen: UIColor { return UIColor(250, 240, 230) }
+    static var mediumAquamarine: UIColor { return UIColor(102, 205, 170) }
+    static var mediumBlue: UIColor { return UIColor(0, 0, 205) }
+    static var mediumOrchid: UIColor { return UIColor(186, 85, 211) }
+    static var mediumPurple: UIColor { return UIColor(147, 112, 219) }
+    static var mediumSeaGreen: UIColor { return UIColor(60, 179, 113) }
+    static var mediumSlateBlue: UIColor { return UIColor(123, 104, 238) }
+    static var mediumSpringGreen: UIColor { return UIColor(0, 250, 154) }
+    static var mediumTurquoise: UIColor { return UIColor(72, 209, 204) }
+    static var mediumVioletRed: UIColor { return UIColor(199, 21, 133) }
+    static var midnightBlue: UIColor { return UIColor(25, 25, 112) }
+    static var mintCream: UIColor { return UIColor(245, 255, 250) }
+    static var mistyRose: UIColor { return UIColor(255, 228, 225) }
+    static var moccasin: UIColor { return UIColor(255, 228, 181) }
+    static var navajoWhite: UIColor { return UIColor(255, 222, 173) }
+    static var oldLace: UIColor { return UIColor(253, 245, 230) }
+    static var oliveDrab: UIColor { return UIColor(107, 142, 35) }
+    static var orangeRed: UIColor { return UIColor(255, 69, 0) }
+    static var orchid: UIColor { return UIColor(218, 112, 214) }
+    static var paleGoldenRod: UIColor { return UIColor(238, 232, 170) }
+    static var paleGreen: UIColor { return UIColor(152, 251, 152) }
+    static var paleTurquoise: UIColor { return UIColor(175, 238, 238) }
+    static var paleVioletRed: UIColor { return UIColor(219, 112, 147) }
+    static var papayaWhip: UIColor { return UIColor(255, 239, 213) }
+    static var peachPuff: UIColor { return UIColor(255, 218, 185) }
+    static var peru: UIColor { return UIColor(205, 133, 63) }
+    static var pink: UIColor { return UIColor(255, 192, 203) }
+    static var plum: UIColor { return UIColor(221, 160, 221) }
+    static var powderBlue: UIColor { return UIColor(176, 224, 230) }
+    static var rosyBrown: UIColor { return UIColor(188, 143, 143) }
+    static var royalBlue: UIColor { return UIColor(65, 105, 225) }
+    static var saddleBrown: UIColor { return UIColor(139, 69, 19) }
+    static var salmon: UIColor { return UIColor(250, 128, 114) }
+    static var sandyBrown: UIColor { return UIColor(244, 164, 96) }
+    static var seaGreen: UIColor { return UIColor(46, 139, 87) }
+    static var seaShell: UIColor { return UIColor(255, 245, 238) }
+    static var sienna: UIColor { return UIColor(160, 82, 45) }
+    static var skyBlue: UIColor { return UIColor(135, 206, 235) }
+    static var slateBlue: UIColor { return UIColor(106, 90, 205) }
+    static var slateGray: UIColor { return UIColor(112, 128, 144) }
+    static var slateGrey: UIColor { return UIColor(112, 128, 144) }
+    static var snow: UIColor { return UIColor(255, 250, 250) }
+    static var springGreen: UIColor { return UIColor(0, 255, 127) }
+    static var steelBlue: UIColor { return UIColor(70, 130, 180) }
+    static var tan: UIColor { return UIColor(210, 180, 140) }
+    static var thistle: UIColor { return UIColor(216, 191, 216) }
+    static var tomato: UIColor { return UIColor(255, 99, 71) }
+    static var turquoise: UIColor { return UIColor(64, 224, 208) }
+    static var violet: UIColor { return UIColor(238, 130, 238) }
+    static var wheat: UIColor { return UIColor(245, 222, 179) }
+    static var whiteSmoke: UIColor { return UIColor(245, 245, 245) }
+    static var yellowGreen: UIColor { return UIColor(154, 205, 50) }
 }
